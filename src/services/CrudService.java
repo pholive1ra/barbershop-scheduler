5bf856577b71bb856ee4332fbf43c947cfd6974e
Pedro <pedrohenrqdev@gmail.com>
2026-05-18 16:56:10 -0300

    feat: added interface

---
+++ src/services/CrudService.java
@@ -0,0 +1,10 @@
+package services;
+
+import java.util.List;
+
+public interface CrudService<T> {
+    void cadastrar(T entidade);
+    List<T> listar();
+    void remover (T entidade);
+    T buscar (String nome);
+}
